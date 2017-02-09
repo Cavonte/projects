275639960271f6c2e5e63319e0b15c8414b6269f
cavonte <edouard.12@live.ca>
2017-02-09 06:18:10 -0500

    Navigation Bar

--- js/parallax.js
+++ js/parallax.js
@@ -7,19 +7,21 @@
 
 $(window).scroll(function(){
 
-var wScroll= $(this).scrollTop();	//variave which tracks how far we are
+var wScroll= $(this).scrollTop();//variave which tracks how far we are
+var minus=500;	
 //from the top
-
 	
-	if (wScroll > $('#wall_1').offset().top && wScroll < $('#content_1').offset().top ) {
+	if (wScroll > $('#wall_1').offset().top && wScroll < $('#content_1').offset().top || wScroll ==0 ) {
 			console.log("current section is : wall_1" );
-
-
+		$('#navHome').addClass('isActive');
+	}
+	else {
+		$('#navHome').removeClass('isActive');
 	}
 	//when you reach half of the previous wall
-	if (wScroll > ($('#content_1').offset().top-500) && wScroll < $('#wall_2').offset().top) {
+	if (wScroll > ($('#content_1').offset().top-minus) && wScroll < $('#wall_2').offset().top) {
 		console.log("current section is : content_1" );
-		var incre=0;
+		$('#navSkills').addClass('isActive');// hightlight the home element when the detected section is home
 		$('.content_1-transition').each(function(i){
 			setTimeout(function(){
 			$('.content_1-transition').eq(i).addClass('is-showing');
@@ -34,6 +36,7 @@ var wScroll= $(this).scrollTop();	//variave which tracks how far we are
 		});
 
 	} else {
+		$('#navSkills').removeClass('isActive');// remove the highlight
 		$('.content_1-transition').each(function(i){
 			setTimeout(function(){
 			$('.content_1-transition').eq(i).removeClass('is-showing');
@@ -47,8 +50,9 @@ var wScroll= $(this).scrollTop();	//variave which tracks how far we are
 		});
 	}
 
-	if (wScroll > ($('#wall_2').offset().top-500) && wScroll < $('#content_2').offset().top ) {
+	if (wScroll > ($('#wall_2').offset().top-minus) && wScroll < $('#content_2').offset().top ) {
 			console.log("current section is : wall_2" );
+		$('#navExperience').addClass('isActive');// hightlight the home element when the detected section is home
 	$('.content_2-transition').each(function(i){
 			setTimeout(function(){
 			$('.content_2-transition').eq(i).addClass('is-showing');
@@ -56,6 +60,7 @@ var wScroll= $(this).scrollTop();	//variave which tracks how far we are
 		});
 		
 	} else {
+		$('#navExperience').removeClass('isActive');
 		$('.content_2-transition').each(function(i){
 			setTimeout(function(){
 			$('.content_2-transition').eq(i).removeClass('is-showing');
@@ -63,24 +68,34 @@ var wScroll= $(this).scrollTop();	//variave which tracks how far we are
 		});
 	}
 
-	if (wScroll > ($('#content_2').offset().top-500) && wScroll < $('#wall_3').offset().top) {
-		console.log("current section is : content_2" );
+	if (wScroll > ($('#content_2').offset().top-minus) && wScroll < $('#wall_3').offset().top) {
+		// console.log("current section is : content_2" );
+		$('#navProjects').addClass('isActive');
 		$('.content_3-transition').each(function(i){
 			setTimeout(function(){
 			$('.content_3-transition').eq(i).addClass('is-showing');
 		}, (700 * (Math.exp(i * 0.14))) - 700);			
 		});
 	} else {
+		$('#navProjects').removeClass('isActive');
 		$('.content_3-transition').removeClass('is-showing');
 	}
 
 	if (wScroll > $('#wall_3').offset().top && wScroll < $('#content_3').offset().top ) {
-		console.log("current section is : wall_3" );
+		// console.log("current section is : wall_3" );
+		$('#navGallery').addClass('isActive');
+	}else{
+		$('#navGallery').removeClass('isActive');
 	}
 
 	if (wScroll > $('#content_3').offset().top && wScroll < $('#wall_4').offset().top) {
-		console.log("current section is : content_3" );
+		// console.log("current section is : content_3" );
+		// $('#navGallery').addClass('isActive');
+	}else{
+		// $('#navGallery').removeClass('isActive');
 	}
+	
+
 
 	// if (wScroll > $('#wall_4').offset().top) {
 	// 	console.log("current section is : wall_4" );
