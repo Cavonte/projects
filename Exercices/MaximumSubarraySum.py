46f72f17f7b0f58a13764da895c9d21469edb871
cavonte <edouard.12@live.ca>
2019-01-24 18:43:56 -0500

    Max Subarray problem

---
+++ Exercices/MaximumSubarraySum.py
@@ -0,0 +1,45 @@
+# Given an array of integer(positive and negative) find the subarray with the maximum sum
+
+# Kadanes algorithm
+def findMaximumSubarraySum(inputArray):
+    maxSoFar = 0
+    maxAthisIndex = 0
+    counter = 0
+    start  = 0
+    temp = 0
+    end = 0
+
+    for i in range(0, len(inputArray)):
+        # Add the current element to the max so far
+        maxAthisIndex = maxAthisIndex + inputArray[i]
+
+        # if you encouter an element that set the max so far below 0 then reset
+        if maxAthisIndex < 0:
+            maxAthisIndex = 0
+            start = i + 1
+
+        # if the current maxAthisIndex then replace the current maximum
+        elif maxSoFar < maxAthisIndex:
+            maxSoFar = maxAthisIndex
+            end = i
+
+    print(start,end)
+    return maxSoFar
+input = [-1,2,4,-3,5,2,-5,2]
+print(findMaximumSubarraySum(input))
+
+a = [-13, -3, -25, -20, -3, -16, -23, -12, -5, -22, -15, -4, -7]
+print(findMaximumSubarraySum(a))
+
+b = a = [-2, -3, 4, -1, -2, 1, 5, -3]
+print(findMaximumSubarraySum(b))
+
+
+
+
+
+
+
+# divide and conquer
+
+
